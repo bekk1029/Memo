8d2ad11a2a9ac58b3fa231cbb5f655094d904b2e
Baljinnyam Buyanjargal <23LP8560@G301-CC0171.local>
2023-12-04 14:53:23 +0800

    unit

---
+++ src/app/components/Main.js
@@ -0,0 +1,56 @@
+export function Main() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center bg-gray-800">
+      <div className="flex flex-wrap w-[650px] gap-[10px]">
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+        <div className="border-2 border-gray-100 border-solid w-[150px] h-[150px] ">
+          <img src="bL.webp" className="w-full h-full  bg-cover bg-center" />
+        </div>
+      </div>
+    </div>
+  );
+}
